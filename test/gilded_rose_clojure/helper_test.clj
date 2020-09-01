32d8e0a1fed0c96d4f54b9d07f7c5a6078cc8f22
Seamus Riordan <seamus@riordan.io>
2020-09-01 07:09:12 -0500

    Split up helper tests to individual tests with single assertions

--- test/gilded_rose_clojure/helper_test.clj
+++ test/gilded_rose_clojure/helper_test.clj
@@ -4,18 +4,22 @@
             ))
 
 
-(deftest dec-keep-positive-tests
-  (testing "dec-keep-positive"
-    (testing "1 goes to 0"
-      (is (= 0 (dec-keep-positive 1))))
-    (testing "0 stays at 0"
-      (is (= 0 (dec-keep-positive 0))))
+(deftest dec-keep-positive-1-goes-to-0
+  (testing "dec-keep-positive 1 goes to 0"
+    (is (= 0 (dec-keep-positive 1)))
+    ))
+
+(deftest dec-keep-positive-0-stays-at-0
+  (testing "dec-keep-positive 0 stays at 0"
+    (is (= 0 (dec-keep-positive 0)))
     ))
 
 (deftest inc-keep-below-tests
-  (testing "inc-keep-below"
-    (testing "1 max of 50 goes to 2"
-      (is (= 2 (inc-keep-below 50 1))))
-    (testing "2 max of 2 stays at 2"
-      (is (= 2 (inc-keep-below 2 2))))
+  (testing "inc-keep-below 1 with max of 50 goes to 2"
+    (is (= 2 (inc-keep-below 50 1)))
+    ))
+
+(deftest inc-keep-below-2-max-of-2-stays-at-max
+  (testing "inc-keep-below 2 with max of 2 stays at 2"
+    (is (= 2 (inc-keep-below 2 2)))
     ))
